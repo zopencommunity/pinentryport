c7d5f9c946eb1bc50d4e0643ca9d3eb2f92bcda7
Haritha D <harithamma.d@ibm.com>
2023-05-18 02:12:24 -0400

    cleanup cicd

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pinentryport.git'), string(name: 'PORT_DESCRIPTION', value: 'pinentry is a small collection of dialog programs that allow GnuPG to read passphrases and PIN numbers in a secure manner.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[P' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pinentryport.git'), string(name: 'PORT_DESCRIPTION', value: 'pinentry is a small collection of dialog programs that allow GnuPG to read passphrases and PIN numbers in a secure manner.' )]
   }
 }
