bd90bba8541bbee9bd29fb4dd0b570f6668113de
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:47 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/pinentryport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/pinentryport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pinentryport.git'), string(name: 'PORT_DESCRIPTION', value: 'pinentry is a small collection of dialog programs that allow GnuPG to read passphrases and PIN numbers in a secure manner.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/pinentryport.git'), string(name: 'PORT_DESCRIPTION', value: 'pinentry is a small collection of dialog programs that allow GnuPG to read passphrases and PIN numbers in a secure manner.' )]
   }
 }
